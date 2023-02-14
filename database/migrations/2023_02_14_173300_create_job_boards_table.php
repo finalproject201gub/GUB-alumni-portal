396c2c7d4d55ebe8d2a49f055bf143ca19dd253d
Md Tawfique Hossain <md.tawfiquehossain@gmail.com>
2023-02-14 23:56:02 +0600

    feature(job-board): model, controller, migration file create

---
+++ database/migrations/2023_02_14_173300_create_job_boards_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobBoardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('job_boards', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->text('title');
+            $table->text('description');
+            $table->string('image');
+            $table->json('tags');
+            $table->string('job_type')->comment('intern, fulltime, part-time');
+            $table->double('salary');
+            $table->string('location');
+            $table->date('application_deadline');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('job_boards');
+    }
+}

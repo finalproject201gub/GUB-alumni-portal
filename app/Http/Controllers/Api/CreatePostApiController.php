c6c2eca33a050b4a507dd36aa7c21d22629efdd0
roven <ashah3562@gmail.com>
2023-03-23 21:56:17 +0600

    changes(timeline-post): Moved apis to web.php

--- app/Http/Controllers/Api/CreatePostApiController.php
+++ app/Http/Controllers/Api/CreatePostApiController.php
@@ -6,18 +6,14 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\PostRequest;
 use App\Models\Post;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Auth;
 
 class CreatePostApiController extends Controller
 {
     public function __invoke(PostRequest $request, Post $post)
     {
         try {
-            // authenticate user id
-            // $request->merge(['user_id' => auth()->user()->id]);
-
             $post->fill([
-                'user_id' => 1,
+                'user_id' => auth()->user()->id,
                 'content' => $request->content,
             ])->save();
 
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Api\CreatePostApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,10 +16,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
-
-Route::prefix('v1')->group(function () {
-    Route::group(['prefix'=> 'posts'], function () {
-        Route::post('store', CreatePostApiController::class);
-    });
 });

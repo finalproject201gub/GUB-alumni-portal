100f67af854420336ae3fc9cbe2aafe746ff94d9
Md Tawfique Hossain <md.tawfiquehossain@gmail.com>
2023-03-23 22:41:18 +0600

    Feat: Name show in sidebar

--- resources/views/skeleton/admin/includes/sidebar.blade.php
+++ resources/views/skeleton/admin/includes/sidebar.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ url('/admin/dashboard') }}" class="brand-link">
-      <img src="{{ asset('img/logo.jpeg') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="{{ asset('img/logo.jpeg') }}" alt="image here" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Alumni Portal</span>
     </a>
 
@@ -13,7 +13,7 @@
           <img src="{{ asset('img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">{{ auth()->user()->name }}</a>
         </div>
       </div>
 

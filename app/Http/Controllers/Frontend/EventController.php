a4a89b815e4426088c481e7fab8f380a59afa1ad
roven <shahalam@skylarksoft.com>
2023-06-03 01:25:54 +0600

    show user avatar in single event view

--- app/Http/Controllers/Frontend/EventController.php
+++ app/Http/Controllers/Frontend/EventController.php
@@ -30,10 +30,12 @@ class EventController extends Controller
     }
 
     public function show(Event $event) {
+        $event->load('createdBy');
         $startDate = Carbon::parse($event->start_at)->format('d M Y') ?? '';
         $endDate = Carbon::parse($event->end_at)->format('d M Y') ?? '';
         $totalDuration = Carbon::parse($event->start_at)->diffForHumans($event->end_at, true) ?? '';
         $event->created_by = $event->createdBy->name ?? 'No Name Specified';
+        $event->created_by_avatar = $event->createdBy->avatar ? asset('img/profile/' . $event->createdBy->avatar) : asset('img/avatar.jpg');
         $event->create_time = $event->created_at->diffForHumans() ?? '';
         $event->time = $startDate . ' To ' . $endDate . ' (' . $totalDuration . ')';
         $event->type = Event::EVENT_TYPES[$event->event_type_id] ?? 'No Type Specified';

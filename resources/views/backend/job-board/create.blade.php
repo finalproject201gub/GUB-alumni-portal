775340520e1e304eea4f13805bbf6f3c372a03d6
roven <shahalam@skylarksoft.com>
2023-06-03 01:49:32 +0600

    added validation

--- resources/views/backend/job-board/create.blade.php
+++ resources/views/backend/job-board/create.blade.php
@@ -17,60 +17,94 @@
                                 @method('POST')
                                 <div class="form-group">
                                     <label for="">Company Name</label>
-                                    <input class="form-control" type="text" name="company_name" id="">
+                                    <input class="form-control" type="text" value="{{ old('company_name') }}" name="company_name" id="">
+                                    @error('company_name')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
                                 </div>
                                 <div class="form-group">
-                                    <label for="">Title</label>
-                                    <input class="form-control" type="text" name="title" id="">
+                                    <label for="">Title</label> <span class="text-danger">*</span>
+                                    <input class="form-control" type="text" value="{{ old('title') }}" name="title" id="">
+                                    @error('title')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
                                 </div>
                                 <div class="form-group">
-                                    <label for="">Description</label>
-                                    <textarea class="form-control" rows="3" name="description"
+                                    <label for="">Description</label><span class="text-danger">*</span>
+                                    <textarea class="form-control" rows="3" name="description" value="{{ old('description') }}"
                                               id=""> </textarea>
+                                    @error('description')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
                                 </div>
                                 <div class="form-group">
                                     <label for="">Vacancy</label>
-                                    <input class="form-control" rows="3" name="vacancy" id=""> </input>
+                                    <input class="form-control" rows="3" name="vacancy" id="" value="{{ old('vacancy') }}"
+                                    > </input>
+                                    @error('vacancy')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
                                 </div>
                                 <div class="form-group">
                                     <label for="">Experience Requirements</label>
-                                    <input class="form-control" rows="3" name="experience_requirements"
+                                    <input class="form-control" rows="3" name="experience_requirements" value="{{ old('experience_requirements') }}"
                                            id=""> </input>
+                                    @error('experience_requirements')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
                                 </div>
                                 <div class="form-group">
                                     <label for="">Education Requirements</label>
-                                    <input class="form-control" rows="3" name="education_requirements"
+                                    <input class="form-control" rows="3" name="education_requirements" value="{{ old('education_requirements') }}"
                                            id=""> </input>
+                                    @error('education_requirements')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
                                 </div>
                                 <div class="form-group">
                                     <label for="">Company Information</label>
-                                    <textarea class="form-control" rows="3" name="company_information"
+                                    <textarea class="form-control" rows="3" name="company_information" value="{{ old('company_information') }}"
                                               id=""> </textarea>
+                                    @error('company_information')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
                                 </div>
 {{--                                <div class="form-group">--}}
 {{--                                    <label for="">Attachment(Image only)</label>--}}
 {{--                                    <input class="form-control" type="file" name="image" id="">--}}
 {{--                                </div>--}}
                                 <div class="form-group">
-                                    <label for="">Job Type</label>
+                                    <label for="">Job Type</label><span class="text-danger">*</span>
                                     <select class="form-control" name="job_type">
                                         <option value="">---Select---</option>
-                                        <option value="intern">Intern</option>
-                                        <option value="part-time">Part Time</option>
-                                        <option value="fulltime">Full Time</option>
+                                        <option value="intern" @if (old('job_type') == 'intern') selected @endif>Intern</option>
+                                        <option value="part-time" @if (old('job_type') == 'part-time') selected @endif>Part Time</option>
+                                        <option value="fulltime" @if (old('job_type') == 'fulltime') selected @endif>Full Time</option>
                                     </select>
+                                    @error('job_type')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
                                 </div>
                                 <div class="form-group">
-                                    <label for="">Salary</label>
-                                    <input class="form-control" type="text" name="salary" id="">
+                                    <label for="">Salary</label><span class="text-danger">*</span>
+                                    <input class="form-control" type="text" name="salary" id="" value="{{ old('salary') }}">
+                                    @error('salary')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
                                 </div>
                                 <div class="form-group">
-                                    <label for="">Location</label>
-                                    <input class="form-control" type="text" name="location" id="">
+                                    <label for="">Location</label><span class="text-danger">*</span>
+                                    <input class="form-control" type="text" name="location" id="" value="{{ old('location') }}">
+                                    @error('location')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
                                 </div>
                                 <div class="form-group">
-                                    <label for="">Application Deadline</label>
-                                    <input class="form-control" type="date" name="application_deadline" id="">
+                                    <label for="">Application Deadline</label><span class="text-danger">*</span>
+                                    <input class="form-control" type="date" name="application_deadline" id="" value="{{ old('application_deadline') }}">
+                                    @error('application_deadline')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
                                 </div>
 
                                 <button class="btn btn-primary" type="submit">Submit</button>
@@ -84,60 +118,93 @@
                                 @method('POST')
                                 <div class="form-group">
                                     <label for="">Company Name</label>
-                                    <input class="form-control" type="text" name="company_name" id="">
+                                    <input class="form-control" type="text" value="{{ old('company_name') }}" name="company_name" id="">
+                                    @error('company_name')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
                                 </div>
                                 <div class="form-group">
-                                    <label for="">Title</label>
-                                    <input class="form-control" type="text" name="title" id="">
+                                    <label for="">Title</label><span class="text-danger">*</span>
+                                    <input class="form-control" type="text" value="{{ old('title') }}" name="title" id="">
+                                    @error('title')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
                                 </div>
                                 <div class="form-group">
-                                    <label for="">Description</label>
-                                    <textarea class="form-control" rows="3" name="description"
+                                    <label for="">Description</label><span class="text-danger">*</span>
+                                    <textarea class="form-control" rows="3" name="description" value="{{ old('description') }}"
                                               id=""> </textarea>
+                                    @error('description')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
                                 </div>
                                 <div class="form-group">
                                     <label for="">Vacancy</label>
-                                    <input class="form-control" rows="3" name="vacancy" id=""> </input>
+                                    <input class="form-control" rows="3" name="vacancy" id="" value="{{ old('vacancy') }}" > </input>
+                                    @error('vacancy')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
                                 </div>
                                 <div class="form-group">
                                     <label for="">Experience Requirements</label>
-                                    <input class="form-control" rows="3" name="experience_requirements"
+                                    <input class="form-control" rows="3" name="experience_requirements" value="{{ old('experience_requirements') }}"
                                            id=""> </input>
+                                    @error('experience_requirements')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
                                 </div>
                                 <div class="form-group">
                                     <label for="">Education Requirements</label>
-                                    <input class="form-control" rows="3" name="education_requirements"
+                                    <input class="form-control" rows="3" name="education_requirements" value="{{ old('education_requirements') }}"
                                            id=""> </input>
+                                    @error('education_requirements')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
                                 </div>
                                 <div class="form-group">
                                     <label for="">Company Information</label>
-                                    <textarea class="form-control" rows="3" name="company_information"
+                                    <textarea class="form-control" rows="3" name="company_information" value="{{ old('company_information') }}"
                                               id=""> </textarea>
+                                    @error('company_information')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
                                 </div>
 {{--                                <div class="form-group">--}}
 {{--                                    <label for="">Attachment(Image only)</label>--}}
 {{--                                    <input class="form-control" type="file" name="image" id="">--}}
 {{--                                </div>--}}
                                 <div class="form-group">
-                                    <label for="">Job Type</label>
-                                    <select class="form-control" name="job_type">
+                                    <label for="">Job Type</label><span class="text-danger">*</span>
+                                    <select class="form-control" name="job_type" value="{{ old('job_type') }}">
                                         <option value="">---Select---</option>
                                         <option value="intern">Intern</option>
                                         <option value="part-time">Part Time</option>
                                         <option value="fulltime">Full Time</option>
                                     </select>
+                                    @error('job_type')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
                                 </div>
                                 <div class="form-group">
-                                    <label for="">Salary</label>
-                                    <input class="form-control" type="text" name="salary" id="">
+                                    <label for="">Salary</label><span class="text-danger">*</span>
+                                    <input class="form-control" type="text" name="salary" id="" value="{{ old('salary') }}">
+                                    @error('salary')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
                                 </div>
                                 <div class="form-group">
                                     <label for="">Location</label>
-                                    <input class="form-control" type="text" name="location" id="">
+                                    <input class="form-control" type="text" name="location" id="" value="{{ old('location') }}">
+                                    @error('location')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
                                 </div>
                                 <div class="form-group">
-                                    <label for="">Application Deadline</label>
-                                    <input class="form-control" type="date" name="application_deadline" id="">
+                                    <label for="">Application Deadline</label><span class="text-danger">*</span>
+                                    <input class="form-control" type="date" name="application_deadline" id="" value="{{ old('application_deadline') }}">
+                                    @error('application_deadline')
+                                        <div class="text-danger">{{ $message }}</div>
+                                    @enderror
                                 </div>
 
                                 <button class="btn btn-primary" type="submit">Submit</button>

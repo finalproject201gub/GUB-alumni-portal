6cb65209f220b4db6ee908551d22f86467f1f0b1
roven <shahalam@skylarksoft.com>
2023-06-03 00:03:53 +0600

    show validation message in registration page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -35,42 +35,54 @@
 
                     <div class="row">
                         <div class="form-group col-md-6 mt-1">
-                            <x-label for="name" :value="__('Name')"/>
+                            <x-label for="name" :value="__('Name')"/> <span class="text-danger">*</span>
 
                             <input id="name" class="form-control" type="text" name="name"
-                                   :value="old('name')" required
+                                   value="{{ old('name') }}"
                                    autofocus placeholder="Full Name"/>
+                            @error('name')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
                         </div>
 
                         <!-- Email Address -->
                         <div class="form-group col-md-6 mt-1">
-                            <x-label for="email" :value="__('Email')"/>
+                            <x-label for="email" :value="__('Email')"/> <span class="text-danger">*</span>
 
                             <input id="email" class="form-control" type="email" name="email"
-                                   :value="old('email')"
-                                   required placeholder="yourname@gmail.com"/>
+                                   value="{{ old('email') }}"
+                                 placeholder="yourname@gmail.com"/>
+                            @error('email')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
                         </div>
                     </div>
 
                     <div class="row">
                         <div class="form-group col-md-6 mt-1">
-                            <x-label for="password" :value="__('Password')"/>
+                            <x-label for="password" :value="__('Password')"/> <span class="text-danger">*</span>
 
                             <input id="password" class="form-control"
                                    type="password"
                                    name="password"
-                                   required autocomplete="new-password"
+                                 autocomplete="new-password"
                                    placeholder="Your Password"/>
-                        </div>
+                                   @error('password')
+                                        <span class="text-danger">{{ $message }}</span>
+                                   @enderror
+                                </div>
 
                         <!-- Confirm Password -->
                         <div class="form-group col-md-6 mt-1">
-                            <x-label for="password_confirmation" :value="__('Confirm Password')"/>
+                            <x-label for="password_confirmation" :value="__('Confirm Password')"/> <span class="text-danger">*</span>
 
                             <input id="password_confirmation" class="form-control"
                                    type="password"
-                                   name="password_confirmation" required
+                                   name="password_confirmation"
                                    placeholder="Confirm Your Password"/>
+                                   @error('password_confirmation')
+                                        <span class="text-danger">{{ $message }}</span>
+                                   @enderror
                         </div>
                     </div>
 
@@ -79,57 +91,76 @@
 
                     <div class="row">
                         <div class="form-group col-md-6 mt-1">
-                            <label for="batch_number">Batch No.</label>
+                            <label for="batch_number">Batch No.</label> <span class="text-danger">*</span>
 
                             <select style="width: 100%;" class="form-control" name="batch_number" id="batch_number">
-                                <option>--Select--</option>
+                                <option value="" disabled>Select your option</option>
                                 @foreach(generateBatchNumbers() as $key => $batchNumber)
-                                    <option value="{{ $key }}">{{ $batchNumber }}</option>
+                                    <option @if (old('batch_number') == $key) selected @endif value="{{ $key }}">
+                                        {{ $batchNumber }}
+                                    </option>
                                 @endforeach
                             </select>
+                            @error('batch_number')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
                         </div>
 
                         <div class="form-group col-md-6 mt-1">
-                            <label for="passing_year">Passing Year.</label>
+                            <label for="passing_year">Passing Year.</label> <span class="text-danger">*</span>
 
                             <select style="width: 100%;" class="form-control" name="passing_year" id="passing_year">
-                                <option>--Select--</option>
+                                <option value="" disabled>Select your option</option>
                                 @foreach(generatePassingYears() as $key => $passingYear)
-                                    <option value="{{ $key }}">{{ $passingYear }}</option>
+                                    <option @if (old('passing_year') == $key) selected @endif value="{{ $key }}">{{ $passingYear }}</option>
                                 @endforeach
                             </select>
+                            @error('passing_year')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
                         </div>
                     </div>
 
 
                     <div class="row">
                         <div class="form-group col-md-6 mt-1">
-                            <label for="department_id">Department</label>
+                            <label for="department_id">Department</label> <span class="text-danger">*</span>
 
                             <select style="width: 100%;" class="form-control" name="department_id" id="department_id">
-                                <option>--Select--</option>
+                                <option value="" disabled>Select your option</option>
                                 @foreach(getDepartments() as $key => $department)
-                                    <option value="{{ $key }}">{{ $department }}</option>
+                                    <option @if (old('department_id') == $key) selected @endif value="{{ $key }}">{{ $department }}</option>
                                 @endforeach
                             </select>
+                            @error('department_id')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
                         </div>
 
                         <div class="form-group col-md-6 mt-1">
-                            <label for="student_id_number">Student ID</label>
+                            <label for="student_id_number">Student ID</label> <span class="text-danger">*</span>
 
                             <input style="width: 100%;" class="form-control" type="text"
+                                    value="{{ old('student_id_number') }}"
                                    name="student_id_number" id="student_id_number"
                                    placeholder="Eg. 1791015123">
+                            @error('student_id_number')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
                         </div>
                     </div>
 
                     <div class="row">
                         <div class="form-group col-md-6 mt-1">
-                            <label for="phone">Phone</label>
+                            <label for="phone">Phone</label> <span class="text-danger">*</span>
 
                             <input style="width: 100%;" class="form-control" type="text"
                                    name="phone" id="phone"
+                                    value="{{ old('phone') }}"
                                    placeholder="Eg. 01711111111">
+                                @error('phone')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
                         </div>
 
                         <div class="form-group col-md-6 mt-1">
@@ -137,6 +168,7 @@
 
                             <input style="width: 100%;" class="form-control" type="text"
                                    name="address" id="address"
+                                    value="{{ old('address') }}"
                                    placeholder="Eg. Shewrapara, Mirpur, Dhaka">
                         </div>
                     </div>
